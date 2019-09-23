fb3a14609f4170ac7638e5423c70c5d7f444e5da
Andrius Simanaitis <eragonas5@gmail.com>
2019-09-23 23:02:44 +0300

    Sutvarkyti konstantų pavadinimai, pridėtas serverio palaikymas

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,26 +5,89 @@
  */
 
 /**
- * localStorage item pavadinimas
+ * port'as naudojamas, norint jungtis į serverį
+ * @type {number}
  */
-const name = 'clients';
-let storage = {};
+const Port = 35000;
+
+/**
+ * pirminis serverio url
+ * @type {string}
+ */
+const Url = `http://localhost:${Port}`;
+
+/**
+ * {string} localStorage item pavadinimas
+ */
+const Name = 'clients';
 /**
  * @type {Client[]}
  */
 let data = [];
 
-const fields = {
+/**
+ * Specialistų darbo pobūdžio sąrašas
+ * @type {{pratesimas: string, isdavimas: string, uzsakymas: string, registracija: string}}
+ */
+const Fields = {
     registracija: 'Registracija',
     isdavimas: 'Išdavimas',
     pratesimas: 'Pratęsimas',
     uzsakymas: 'Užsakymas',
 };
-function loadStorage() {
-    storage = localStorage.getItem(name);
-    if (storage != null) {
-        data = JSON.parse(storage).sort((a, b) => b.register - a.register);
+/**
+ * norint naudoti serverį, o ne localStorage, pakeisti į false
+ * @type {boolean}
+ */
+const UseLocal = true;
+/**
+ * užkrauna duomenis iš localStorge arba serverio
+ */
+async function loadData() {
+    if (UseLocal) {
+        let storage = localStorage.getItem(Name);
+        if (storage != null) {
+            data = JSON.parse(storage).sort((a, b) => b.register - a.register);
+        } else {
+            throw new Error('No localStorage');
+        }
     } else {
-        data = [];
+        let output = await fetch(Url + '/getdata');
+        let json = await output.json();
+        console.log(json);
+        data = json.map(
+            e => new Client(e.id, e.subject, e.register, e.unique, e.served)
+        );
+    }
+}
+
+/**
+ * padeda duomenis į localstorage arba serverį
+ * @return {boolean} success?
+ */
+async function putData() {
+    if (UseLocal) {
+        let storage = localStorage.getItem(Name);
+        if (storage != null) {
+            localStorage.setItem(Name, JSON.stringify(data));
+            return true;
+        } else {
+            throw new Error('No localStorage');
+        }
+    } else {
+        let output = await fetch(Url + '/putdata', {
+            method: 'PUT',
+            body: JSON.stringify({ clients: data }),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        let json = await output.json();
+        return (
+            json.success !== undefined &&
+            Object.keys(json).includes('success') &&
+            json.success
+        );
     }
 }
